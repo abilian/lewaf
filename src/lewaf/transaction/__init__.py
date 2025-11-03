2dc2556580d482befcf6ef95475f07c6e1ba51bd
Stefane Fermigier <sf@abilian.com>
2025-11-03 14:15:28 +0100

    refactor(rules,transaction): add type hints and improve code clarity

--- src/lewaf/transaction/__init__.py
+++ src/lewaf/transaction/__init__.py
@@ -3,17 +3,22 @@ from __future__ import annotations
 from urllib.parse import parse_qs
 
 from lewaf.primitives.collections import TransactionVariables
+from typing import Dict, Optional, Union, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from lewaf.integration import WAF
+    from lewaf.rules import Rule
 
 
 class Transaction:
-    def __init__(self, waf, id):
+    def __init__(self, waf: WAF, id: str):
         self.id = id
         self.waf = waf
         self.variables = TransactionVariables()
         self.interruption: dict[str, str | int] | None = None
         self.current_phase = 0
 
-    def process_uri(self, uri, method):
+    def process_uri(self, uri: str, method: str):
         self.variables.request_uri.set(uri)
         self.variables.request_method.set(method)
         # Set default HTTP protocol version
@@ -24,15 +29,15 @@ class Transaction:
                 for value in values:
                     self.variables.args.add(key, value)
 
-    def process_request_headers(self):
+    def process_request_headers(self) -> Optional[Dict[str, Union[str, int]]]:
         self.current_phase = 1
         self.waf.rule_group.evaluate(1, self)
         return self.interruption
 
-    def process_request_body(self):
+    def process_request_body(self) -> Optional[Dict[str, Union[str, int]]]:
         self.current_phase = 2
         self.waf.rule_group.evaluate(2, self)
         return self.interruption
 
-    def interrupt(self, rule):
+    def interrupt(self, rule: Rule):
         self.interruption = {"rule_id": rule.id, "action": "deny"}
